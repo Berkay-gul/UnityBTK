ddce169e48cd3d1a8af8b06d73a6c5b03ad6c07a
Berkay Gül <berkay20031794@hotmail.com>
2025-08-13 14:18:53 +0300

    Movment Methotds
    Learned most used movement methods.

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -1,26 +1,127 @@
+using System.Collections;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class MovementManager : MonoBehaviour
 {
-    private Rigidbody rb;
-    public float force = 10f;
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();                     //Ayni nesnedeki rigidbody componentine erismemeizi saglayan kod.
-    }
-    void Update()
 
+    // bir noktadan baska bir noktaya courouitine kullanarak hareket etme.
+    public Transform target;
+    public float moveduration = 2f;
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))                // space tusuna basildiginda aktiflesecek sorgu.
+        StartCoroutine(MoveRouitine(target.position, moveduration));
+    }
+
+
+    
+        IEnumerator MoveRouitine(Vector3 targetpos, float time)
         {
-            rb.AddForce(Vector3.up * force);                // yukari yonlu guc uygular.
+            Vector3 startposition = transform.position;
+            float passedtime = 0f;
+            while (passedtime < time)
+            {
+                transform.position = Vector3.Lerp(transform.position, target.position, passedtime / time);
+                passedtime += Time.deltaTime;
+                yield return null;// 2 kare arasindaki gecen sure kadar beklemeyek icin kullaniliyor.
+            }
+            // hedef pozisyona ulasma.
+            transform.position = targetpos;
         }
-        //rb.linearVelocity = new Vector3(5, 0, 0);        // ulastigimiz rigidbody nin ivme parametresi ile ilgili islem yapiyoruz.
-    }
+    
+    
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* 
+        //2 nokta arasinda smooth harekete yariyan yapi.
+
+        public Transform target;
+        public float speed = 2f;
+
+        void Update()
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);// belirlenen hedefe smooth bir sekilde hareket edilmesi icin kullanilir.
+        }
+     */
+
+
+
+
+
+    /*     //Vector3.MoveTowards duz bir cizgide sabit bir hiz saglar.
+
+        public Transform[] Targets;
+        public float speed = 5f;
+
+        int currentindex = 0;
+        private void Update()// sectigimiz hedefler arasinda teker teker gitmesini saglayan kod.
+        {
+            if (currentindex < Targets.Length)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Targets[currentindex].position, speed * Time.deltaTime);
+                if (transform.position == Targets[currentindex].position)
+                {
+                    currentindex++;
+                }
+            }
+
+        }
+            */
+
+
+
+
+    /* 
+private Rigidbody rb;
+public float force = 10f;
+private void Awake()
+{
+rb = GetComponent<Rigidbody>();                     //Ayni nesnedeki rigidbody componentine erismemeizi saglayan kod.
+}
+void Update()
+
+{
+if (Input.GetKeyDown(KeyCode.Space))                // space tusuna basildiginda aktiflesecek sorgu.
+{
+rb.AddForce(Vector3.up * force,ForceMode.Force);                // yukari yonlu guc uygular.
+/* 
+FORCE MODS        (ForceMode.*****)
+-force = Devamli kuvvet uygular.
+-Impulse = Anlik kuuvet uygular. Hizda ani bir degisiklik yapar. 
+-VelocityChange = Nesnenin hizimi dogrudan degistirir. 
+-Acceleration = Kutleden bagimsiz bir ivme uygular.
+
+
+
+
+//rb.linearVelocity = new Vector3(5, 0, 0);        // ulastigimiz rigidbody nin hiz parametresi ile ilgili islem yapiyoruz.
+
+
+
+}
+}
+
+
+
+*/
 
 
 
